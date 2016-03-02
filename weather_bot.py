060530ed6e531a6f9406f6445397692c22992afe
JGabrielFreitas <jgabrielafreitas@gmail.com>
2016-03-02 16:13:34 -0300

    uploading weather bot alpha 0.0.1

--- weather_bot.py
+++ weather_bot.py
@@ -5,56 +5,67 @@ import json
 import schedule
 import time
 import emoji
+import traceback
 
 DEV_API_KEY = "44db6a862fba0b067b1930da0d769e98"
 TOKEN = '213675554:AAEHHFkB-NCFt0evRhfoodeL3SOIIEfDGNQ'
 dest = '@clima_rio'
-creator = '@jgabrielfreitas'
+shut_down_alert = '@shut_down_clima_rio'
 
 bot = telebot.TeleBot(TOKEN)
 
+def kelvin_to_celsius(kelvin):
+    return ("%.1f" % (kelvin - 273) )
+
+
 def job():
-    city = "rio de janeiro"
-    country = "br"
-    url = "http://api.openweathermap.org/data/2.5/weather?q={0},{1}&lang=pt&appid={2}".format(city, country, DEV_API_KEY)
-    response = requests.request("GET", url)
-    data = json.loads(response.text)
-    list_of_weathers = data['weather']
-    current_weather = list_of_weathers[0]
-    main_current = current_weather['main']
+    try:
+        city = "rio de janeiro"
+        country = "br"
+        url = "http://api.openweathermap.org/data/2.5/weather?q={0},{1}&lang=pt&appid={2}".format(city, country, DEV_API_KEY)
+        response = requests.request("GET", url)
+        data = json.loads(response.text)
+        list_of_weathers = data['weather']
+        current_weather = list_of_weathers[0]
+        main_current = current_weather['main']
 
-    main = data['main']
+        main = data['main']
 
-    # get description
-    description = current_weather['description']
-    description = description[:1].upper() + description[1:]
+        # get description
+        description = current_weather['description']
+        description = description[:1].upper() + description[1:]
 
-    # emojis
-    cloud = ':cloud:'
-    sun_behind_cloud = ':sun_behind_cloud:'
-    sun = ':black_sun_with_rays:'
-    sun_with_cloud_and_rain = ':white_sun_behind_cloud_with_rain:'
-    rain = ':cloud_with_rain:'
+        # emojis
+        cloud = ':cloud:'
+        sun_behind_cloud = ':sun_behind_cloud:'
+        sun = ':black_sun_with_rays:'
+        sun_with_cloud_and_rain = ':white_sun_behind_cloud_with_rain:'
+        rain = ':cloud_with_rain:'
 
-    emoji_to_show = sun # by default
+        emoji_to_show = sun # default is the sun
 
-    humidity = main['humidity']
-    temp = main['temp']
-    temp_min = main['temp_min']
-    temp_max = main['temp_max'] / 10
-
-    if main_current == 'Clouds':
-        emoji_to_show = ':cloud:'
-    else:
-        # send sun (default) and send message for me
-        # TODO
-        bot.send_message(creator, "description to add: {0}".format(main_current))
+        humidity = main['humidity']
+        temp = main['temp']
+        temp_min = kelvin_to_celsius(main['temp_min'])
+        temp_max = kelvin_to_celsius(main['temp_max'])
 
 
-    # message_to_send = u"{0} - {1} \nTemperatura maxima: sC".format(emoji_to_show, description)
-    bot.send_message(dest, emoji.emojize(u"{0} - {1} \nTemperatura maxima: {2}C".format(emoji_to_show, description, temp_max))) # dest and msg
+        if main_current == 'Clouds':
+            emoji_to_show = ':cloud:'
+        else:
+            # send sun (default) and send message for me
+            bot.send_message(shut_down_alert, "description to add: {0}".format(main_current))
+
+        # bot.send_message(dest, emoji.emojize(u"{0} - {1} \nTemperatura maxima: {2}C".format(emoji_to_show, description, temp_max))) # dest and msg
+        print emoji.emojize("{0} - {1} \nTemperatura maxima: {2} C\nTemperatura minima: {3} C\n\n==============\nCreated by @JGabrielFreitas\nPowered by OpenWeather API".format(emoji_to_show, description, temp_max, temp_min))
+    except:
+        bot.send_message(shut_down_alert, "Dude, your bot is down...:\n\n{0}".format(traceback.format_exc()))
 
 
 
 # run jobs
-job()
+schedule.every().hour.do(job)
+
+while True:
+    schedule.run_pending()
+    # time.sleep(1)
